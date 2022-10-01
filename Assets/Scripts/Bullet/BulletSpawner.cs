084d3de7a9a0e6a18b0b28a71250e70689c3044d
karataskn20 <karataskn20@gmail.com>
2022-10-01 16:41:40 +0300

    Update Scene

--- Assets/Scripts/Bullet/BulletSpawner.cs
+++ Assets/Scripts/Bullet/BulletSpawner.cs
@@ -2,9 +2,30 @@
 
 public class BulletSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject bullet;
-    void Start()
+    [SerializeField] private GameObject playerBullet;
+    [SerializeField] private float nextFire = 0.2F;
+    private float myTime = 0.0F;
+    private float fireDelta = 0.2F;
+
+    void Update()
     {
-        Instantiate(bullet, transform.position, Quaternion.identity);
+        ShootBullets();
+    }
+
+    void ShootBullets()
+    {
+        myTime += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.T) && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            if (playerBullet != null)
+            {
+                Instantiate(playerBullet, transform.position, transform.rotation);
+            }
+            nextFire = nextFire - myTime;
+
+            myTime = 0f;
+        }
     }
 }
