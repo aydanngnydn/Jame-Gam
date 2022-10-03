08c8e8495194858ebac9ea58514bc78d631c4c2f
Aydan Günaydın <aydangunaydin@hotmail.com>
2022-10-03 18:06:00 +0300

    push

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void Settings()
+    {
+        
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
